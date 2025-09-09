957a560634e96d5f2f77d2cddcf3e6672a7bbede
Wesley Prado <wesleyprado.dev@gmail.com>
2025-09-08 21:40:36 -0300

    refactor(core): remove arquivo contendo má prática de mercado

--- src/main/resources/db/migration/V2__insert_admin_user.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO users (id, username, email, password, document, document_type, role, status)
-VALUES ('c32118b1-f73e-4498-bdc1-1356b8a5cbad',
-        'admin',
-        'wesleyprado.dev@gmail.com',
-        '$2a$10$0Od6n0QKAJKYf.SMVbz1geTJSwnguG//KcStaU8JKihl17jwBZAJG',
-        '48142198061',
-        'CPF',
-        'ADMIN',
-        'ACTIVE') ON CONFLICT (username) DO NOTHING;
