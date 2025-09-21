b0cd887bef384f64d233de987a8e39c156b9a730
Wesley Prado <wesleyprado.dev@gmail.com>
2025-09-21 12:04:19 -0300

    fix(user): move redundant links from records to the root of json response

--- src/main/java/com/codemages/Moviee/user/assembler/UserModelAssembler.java
+++ src/main/java/com/codemages/Moviee/user/assembler/UserModelAssembler.java
@@ -3,31 +3,47 @@ package com.codemages.Moviee.user.assembler;
 import com.codemages.Moviee.user.controller.v1.PrivateUserController;
 import com.codemages.Moviee.user.dto.UserResponseDTO;
 import lombok.NonNull;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.stereotype.Component;
 
+import java.util.stream.StreamSupport;
+
 @Component
 public class UserModelAssembler implements
                                 RepresentationModelAssembler<UserResponseDTO,
                                   EntityModel<UserResponseDTO>> {
+
+
+  @NonNull
+  @Override
+  public CollectionModel<EntityModel<UserResponseDTO>> toCollectionModel(
+    @NonNull
+    Iterable<? extends UserResponseDTO> entities
+  ) {
+    Link selfLink = WebMvcLinkBuilder.linkTo( WebMvcLinkBuilder
+        .methodOn( PrivateUserController.class ).getUsers() )
+      .withRel( "users" );
+
+    var models = StreamSupport.stream( entities.spliterator(), false )
+      .map( this::toModel )
+      .toList();
+
+    return CollectionModel.of( models, selfLink );
+  }
 
   @NonNull
   @Override
   public EntityModel<UserResponseDTO> toModel(
     UserResponseDTO dto
   ) {
-    Link selfLink = WebMvcLinkBuilder
-      .linkTo( WebMvcLinkBuilder
-        .methodOn( PrivateUserController.class )
-        .getUser( dto.id() ) )
+    Link self = WebMvcLinkBuilder.linkTo( WebMvcLinkBuilder
+        .methodOn( PrivateUserController.class ).getUserById( dto.id() ) )
       .withSelfRel();
-    Link usersLink = WebMvcLinkBuilder.linkTo( WebMvcLinkBuilder
-        .methodOn( PrivateUserController.class ).getUsers() )
-      .withRel( "users" );
 
-    return EntityModel.of( dto, selfLink, usersLink );
+    return EntityModel.of( dto, self );
   }
 }
