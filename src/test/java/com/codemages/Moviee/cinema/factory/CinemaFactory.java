888c0f58cd38b8742a33eb9b835e376e4029f079
Wesley Prado <wesleyprado.dev@gmail.com>
2025-09-25 11:10:18 -0300

    feat(factory): add methods to create Movie, MovieCredit, and Person entities

--- src/test/java/com/codemages/Moviee/cinema/factory/CinemaFactory.java
+++ src/test/java/com/codemages/Moviee/cinema/factory/CinemaFactory.java
@@ -1,11 +1,16 @@
 package com.codemages.Moviee.cinema.factory;
 
 import com.codemages.Moviee.cinema.movie.Movie;
+import com.codemages.Moviee.cinema.movie.MovieCredit;
 import com.codemages.Moviee.cinema.movie.constant.Genre;
+import com.codemages.Moviee.cinema.movie.constant.MovieRole;
 import com.codemages.Moviee.cinema.movie.test.util.IdGenerator;
+import com.codemages.Moviee.cinema.person.Person;
 import com.codemages.Moviee.cinema.room.Room;
 import com.codemages.Moviee.cinema.session.Session;
 
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.util.List;
 
 public class CinemaFactory {
@@ -15,26 +20,42 @@ public class CinemaFactory {
 
   public static Movie createMovie() {
     return Movie.builder()
+      .title( "Forrest Gump" )
+      .year( 1994 )
+      .durationInMinutes( 142 )
+      .rating( 8 )
+      .build();
+  }
+
+  public static MovieCredit createMovieCredit(MovieRole role) {
+    return MovieCredit.builder().role( role ).build();
+  }
+
+  public static Person createPerson() {
+    return Person.builder()
+      .artisticName( "Tom Hanks" )
+      .realName( "Thomas Jeffrey Hanks" )
+      .dob( ZonedDateTime.of( 1956, 7, 9, 0, 0, 0, 0, ZoneOffset.UTC ) )
+      .about( "An American actor and filmmaker." )
+      .build();
+  }
+
+  public static Movie createMovieMock() {
+    return Movie.builder()
       .id( IdGenerator.nextId( MOVIE_CLASS_NAME ) )
       .title( "Inception" )
       .durationInMinutes( 148 )
       .description( "A mind-bending SCI-FI" )
       .genres( List.of( Genre.SCI_FI, Genre.ACTION, Genre.DRAMA ) )
-      .director( "Christopher Nolan" )
-      .writers( List.of( "Christopher Nolan" ) )
-      .year( 2010 )
       .rating( 8 )
       .build();
   }
 
-  public static Room createRoom() {
-    return Room.builder()
-      .id( IdGenerator.nextId( ROOM_CLASS_NAME ) )
-      .name( "Room 1" )
-      .build();
+  public static Room createRoomMock() {
+    return Room.builder().id( IdGenerator.nextId( ROOM_CLASS_NAME ) ).name( "Room 1" ).build();
   }
 
-  public static Session createSession(Movie movie, Room room) {
+  public static Session createSessionMock(Movie movie, Room room) {
     var now = java.time.ZonedDateTime.now();
     var twoHoursAhead = now.plusHours( 2 );
 
