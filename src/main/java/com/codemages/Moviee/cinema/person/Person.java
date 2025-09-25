ddb7012660ac955067b36a12a1acaffb0bed7d9d
Wesley Prado <wesleyprado.dev@gmail.com>
2025-09-25 11:47:42 -0300

    feat(person): add one-to-many MovieCredit relationship and builder

--- src/main/java/com/codemages/Moviee/cinema/person/Person.java
+++ src/main/java/com/codemages/Moviee/cinema/person/Person.java
@@ -1,10 +1,16 @@
 package com.codemages.Moviee.cinema.person;
 
+import com.codemages.Moviee.cinema.movie.Movie;
+import com.codemages.Moviee.cinema.movie.MovieCredit;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.ZonedDateTime;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "persons", schema = "cinema")
@@ -12,7 +18,7 @@ import java.util.UUID;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+@Builder(builderClassName = "PersonBuilder")
 public class Person {
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
@@ -27,4 +33,44 @@ public class Person {
   private ZonedDateTime dob;
 
   private String about;
+
+  @OneToMany(mappedBy = "person", cascade = {
+    CascadeType.PERSIST, CascadeType.MERGE
+  }, orphanRemoval = true)
+  @Builder.Default
+  private Set<MovieCredit> credits = new HashSet<>();
+
+  public void addCredit(MovieCredit credit) {
+    credits.add( credit );
+    credit.setPerson( this );
+  }
+
+  public void removeCredit(MovieCredit credit) {
+    credits.remove( credit );
+    credit.setPerson( null );
+  }
+
+  private void setCredits(Set<MovieCredit> credits) {
+    this.credits = credits;
+  }
+
+  @Transient
+  public Set<Movie> getMovies() {
+    if ( credits == null ) return Collections.emptySet();
+    return credits.stream().map( MovieCredit::getMovie ).collect( Collectors.toSet() );
+  }
+
+  public static class PersonBuilder {
+    @Singular
+    private Set<MovieCredit> credits;
+
+    private PersonBuilder credits(Set<MovieCredit> credits) {
+      return this;
+    }
+
+    public PersonBuilder credit(MovieCredit credit) {
+      credits.add( credit );
+      return this;
+    }
+  }
 }
