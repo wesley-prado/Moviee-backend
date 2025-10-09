f006258e9948b9423e45d9787dd15d2f21d538da
Wesley Prado <wesleyprado.dev@gmail.com>
2025-10-08 21:45:33 -0300

    refactor: move DataInitializer to higher level package

---
+++ src/main/java/com/codemages/Moviee/core/config/DataInitializer.java
@@ -0,0 +1,76 @@
+package com.codemages.Moviee.core.config;
+
+import com.codemages.Moviee.auth.client.ClientService;
+import com.codemages.Moviee.auth.client.dto.ClientDTO;
+import com.codemages.Moviee.cinema.movie.service.GenreService;
+import com.codemages.Moviee.user.UserService;
+import com.codemages.Moviee.user.constant.DocumentType;
+import com.codemages.Moviee.user.constant.Role;
+import com.codemages.Moviee.user.dto.PrivateUserCreationDTO;
+import com.codemages.Moviee.user.dto.UserResponseDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Slf4j
+@Configuration
+@Profile({ "dev", "test" })
+@RequiredArgsConstructor
+public class DataInitializer {
+  private final UserService userService;
+  private final ClientService clientService;
+  private final GenreService genreService;
+
+  @Bean
+  CommandLineRunner initData() {
+    return args -> {
+      initUsers();
+      initClients();
+      initGenres();
+    };
+  }
+
+  private void initUsers() {
+    if ( !userService.isUsernameTaken( "myuser" ) ) {
+      PrivateUserCreationDTO user = new PrivateUserCreationDTO(
+        "myuser", "user@mail.com",
+        "User1#@@", "336189783",
+        DocumentType.RG.name(),
+        Role.USER
+      );
+      PrivateUserCreationDTO admin = new PrivateUserCreationDTO(
+        "admin", "admin@mail.com",
+        "Admin1#@", "479212910",
+        DocumentType.RG.name(),
+        Role.ADMIN
+      );
+
+      UserResponseDTO regularUser = userService.createPrivateUser( user );
+      UserResponseDTO adminUser = userService.createPrivateUser( admin );
+
+      log.debug( "Created Regular User: {}", regularUser );
+      log.debug( "Created Admin User: {}", adminUser );
+    }
+  }
+
+  private void initClients() {
+    if ( !clientService.existsByClientId( "postman" ) ) {
+      clientService.save( new ClientDTO(
+        "postman",
+        "my_client_secret",
+        "Postman Client",
+        "https://oauth.pstmn.io/v1/callback"
+      ) );
+
+      log.debug( "Created Client: postman" );
+    }
+  }
+
+  private void initGenres() {
+    genreService.insertMissingGenres();
+    log.debug( "Genres initialized" );
+  }
+}
