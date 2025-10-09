82392bb1ecde40cdb14fe8e1b41f0c16604819e4
Wesley Prado <wesleyprado.dev@gmail.com>
2025-10-08 22:26:20 -0300

    feat(genre): add endpoint to retrieve genre by ID and rename method for retrieving all genres

--- src/main/java/com/codemages/Moviee/cinema/movie/controller/PublicGenreController.java
+++ src/main/java/com/codemages/Moviee/cinema/movie/controller/PublicGenreController.java
@@ -1,9 +1,8 @@
 package com.codemages.Moviee.cinema.movie.controller;
 
-import com.codemages.Moviee.cinema.movie.GenreService;
 import com.codemages.Moviee.cinema.movie.assembler.GenreModelAssembler;
-import com.codemages.Moviee.cinema.movie.constant.Genre;
 import com.codemages.Moviee.cinema.movie.dto.GenreResponseDTO;
+import com.codemages.Moviee.cinema.movie.service.GenreService;
 import com.codemages.Moviee.core.constant.ControllerConstant;
 import lombok.AllArgsConstructor;
 import org.springframework.hateoas.CollectionModel;
@@ -11,6 +10,7 @@ import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,11 +22,15 @@ public class PublicGenreController {
   private final GenreModelAssembler genreModelAssembler = new GenreModelAssembler();
 
   @GetMapping
-  public ResponseEntity<CollectionModel<EntityModel<GenreResponseDTO>>> getGenres() {
+  public ResponseEntity<CollectionModel<EntityModel<GenreResponseDTO>>> findAllGenres() {
     return ResponseEntity.ok()
       .contentType( MediaTypes.HAL_JSON )
-      .body( genreModelAssembler.toCollectionModel(
-        genreService.convertToGenreResponseDTO( Genre.values() )
-      ) );
+      .body( genreModelAssembler.toCollectionModel( genreService.findAll() ) );
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<EntityModel<GenreResponseDTO>> findGenreById(@PathVariable Long id) {
+    return ResponseEntity.ok()
+      .body( genreModelAssembler.toModel( genreService.findById( id ) ) );
   }
 }
