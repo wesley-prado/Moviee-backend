19379ef179762e44998f15d97acd45bc1794c7f1
Wesley Prado <wesleyprado.dev@gmail.com>
2025-10-08 22:25:08 -0300

    test(PublicGenreController): add tests for genre retrieval endpoints

---
+++ src/test/java/com/codemages/Moviee/cinema/movie/controller/PublicGenreControllerTest.java
@@ -0,0 +1,98 @@
+package com.codemages.Moviee.cinema.movie.controller;
+
+import com.codemages.Moviee.cinema.factory.CinemaFactory;
+import com.codemages.Moviee.cinema.movie.assembler.GenreModelAssembler;
+import com.codemages.Moviee.cinema.movie.dto.GenreResponseDTO;
+import com.codemages.Moviee.cinema.movie.service.GenreService;
+import com.codemages.Moviee.core.constant.ControllerConstant;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(PublicGenreController.class)
+@Import(GenreModelAssembler.class)
+@AutoConfigureMockMvc(addFilters = false)
+public class PublicGenreControllerTest {
+  private static final String BASE_URL = ControllerConstant.PUBLIC_API_BASE + "/v1/genres";
+
+  @MockitoBean
+  private GenreService genreService;
+
+  @Autowired
+  private MockMvc mvc;
+
+  @ParameterizedTest(name = "Accessing as {0}")
+  @CsvSource({
+    "USER", "MODERATOR", "ADMIN"
+  })
+  @DisplayName("Should return 200 when any type of user try to get all genres")
+  void getAllGenres_AnyTypeOfUser_ShouldReturnOk(String role) throws Exception {
+    List<GenreResponseDTO> genres = List.of(
+      CinemaFactory.createGenreResponseDTO(),
+      CinemaFactory.createGenreResponseDTO(),
+      CinemaFactory.createGenreResponseDTO()
+    );
+    when( genreService.findAll() ).thenReturn( genres );
+
+    GenreResponseDTO genre = genres.getFirst();
+
+    mvc.perform( get( BASE_URL ).with( SecurityMockMvcRequestPostProcessors.user( role )
+        .roles( role ) ) )
+      .andExpect( status().isOk() )
+      .andExpect( content().contentType( "application/hal+json" ) )
+      .andExpect( jsonPath( "$._embedded.genres", Matchers.hasSize( 3 ) ) )
+      .andExpect( jsonPath(
+        "$._embedded.genres[0].id",
+        Matchers.equalTo( genre.id().intValue() )
+      ) )
+      .andExpect( jsonPath( "$._embedded.genres[0].name", Matchers.equalTo( genre.name() ) ) )
+      .andExpect( jsonPath(
+        "$._embedded.genres[1].description",
+        Matchers.equalTo( genre.description() )
+      ) );
+  }
+
+  @Test
+  @DisplayName("Should return 200 when any type of user try to get all genres")
+  @WithAnonymousUser
+  void getAllGenres_AnonymousUser_ShouldReturnOk() throws Exception {
+    List<GenreResponseDTO> genres = List.of(
+      CinemaFactory.createGenreResponseDTO(),
+      CinemaFactory.createGenreResponseDTO(),
+      CinemaFactory.createGenreResponseDTO()
+    );
+    when( genreService.findAll() ).thenReturn( genres );
+
+    GenreResponseDTO genre = genres.getFirst();
+
+    mvc.perform( get( BASE_URL ) )
+      .andExpect( status().isOk() )
+      .andExpect( content().contentType( "application/hal+json" ) )
+      .andExpect( jsonPath( "$._embedded.genres", Matchers.hasSize( 3 ) ) )
+      .andExpect( jsonPath(
+        "$._embedded.genres[0].id",
+        Matchers.equalTo( genre.id().intValue() )
+      ) )
+      .andExpect( jsonPath( "$._embedded.genres[0].name", Matchers.equalTo( genre.name() ) ) )
+      .andExpect( jsonPath(
+        "$._embedded.genres[1].description",
+        Matchers.equalTo( genre.description() )
+      ) );
+  }
+}
