c44bab545813fe5cf6e18ab5aae2281ee033ec5e
Wesley Prado <wesleyprado.dev@gmail.com>
2025-10-20 16:44:48 -0300

    chore(docker): remove unused commands from entrypoint script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
 exec "$@"
-#while inotifywait -r -e modify /app/src/; 
-#do 
-#  ./mvnw compile -o -DskipTests && touch /app/.triggerfile; 
-#done >/dev/null 2>&1 &
-#
-#./mvnw spring-boot:run -Dspring-boot.run.arguments=--logging.level.org.springframework=DEBUG
