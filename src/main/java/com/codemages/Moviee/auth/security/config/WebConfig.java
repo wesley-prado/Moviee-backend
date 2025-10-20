a97fe452bf0b6afe6368623290e43ed1b230d57c
Wesley Prado <wesleyprado.dev@gmail.com>
2025-10-20 16:46:35 -0300

    feat(config): add ForwardedHeaderFilter configuration for handling forwarded headers

---
+++ src/main/java/com/codemages/Moviee/auth/security/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.codemages.Moviee.auth.security.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ForwardedHeaderFilter;
+
+@Configuration
+public class WebConfig {
+
+  @Bean
+  FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
+    FilterRegistrationBean<ForwardedHeaderFilter> bean = new FilterRegistrationBean<>();
+    bean.setFilter( new ForwardedHeaderFilter() );
+    return bean;
+  }
+}
