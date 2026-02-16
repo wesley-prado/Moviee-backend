a33786164a2a4087845da22148fc97a6ad06264f
Wesley Prado <wesleyprado.dev@gmail.com>
2026-02-16 16:53:44 -0300

    test: add transactional support to DatabaseTestContainer for improved test isolation

--- src/test/java/com/codemages/Moviee/DatabaseTestContainer.java
+++ src/test/java/com/codemages/Moviee/DatabaseTestContainer.java
@@ -7,10 +7,12 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 @SpringBootTest
 @AutoConfigureTestEntityManager
+@Transactional
 public abstract class DatabaseTestContainer {
   private static final PostgreSQLContainer<?> CONTAINER = new PostgreSQLContainer<>(
     "postgres:latest" ).withEnv( "POSTGRES_INITDB_ARGS", "-d" );
